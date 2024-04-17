161dea7ba14bd08dd4c4ba05508462f592b2d91c
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2024-04-17 16:38:42 +0800

    Add files via upload

--- src/main/kotlin/Confectionery/Confectionery.kt
+++ src/main/kotlin/Confectionery/Confectionery.kt
@@ -1,8 +1,24 @@
 package Confectionery
 
 enum class Confectionery {
-    NONE,
-    CAKE,
-    SWEET,
-    CUPCACKE
+    CAKE {
+        override val price = 20.0f
+        override fun printName(): String {
+            return "Торт"
+        }
+    },
+    SWEET {
+        override val price = 5.0f
+        override fun printName(): String {
+            return "Конфета"
+        }
+    },
+    CUPCACKE {
+        override val price = 10.0f
+        override fun printName(): String {
+            return "Кекс"
+        }
+    };
+    abstract fun printName(): String
+    abstract val price: Float
 }
